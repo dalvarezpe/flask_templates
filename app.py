a9b3ff3c87b5319d077fae00cf68440f464309ff
dalvarezpe <danniel.alejandroap@gmail.com>
2024-12-18 07:38:54 -0400

    Descripción de los cambios

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template
+import plotly.graph_objs as go
+import plotly.io as pio
 
 app = Flask(__name__)
 
 @app.route('/')
-def run():
-    return render_template('home.html')
+def show_graph():
+    # Datos para el gráfico
+    subjects = ['Physics', 'Maths', 'Chemistry', 'Hindi', 'English']
+    marks = [80, 85, 90, 70, 75]  # Datos ficticios
 
-@app.route('/submit', methods=['POST'])
-def marks():
-    Physics=int(request.form['Physics'])
-    Maths= int(request.form['Maths'])
-    Chemistry = int(request.form['Chemistry'])
-    Hindi = int(request.form['Hindi'])
-    English = int(request.form['English'])
-    result = Physics + Maths + Chemistry + Hindi + English
-    Percentage = result/5
-    return render_template('home.html',Percentage=Percentage)
+    # Crear el gráfico con Plotly
+    fig = go.Figure(data=[go.Bar(x=subjects, y=marks, marker_color='blue')])
+    fig.update_layout(
+        title="Marks Distribution",
+        xaxis_title="Subjects",
+        yaxis_title="Marks",
+        plot_bgcolor="white",
+        margin=dict(l=40, r=40, t=40, b=40)
+    )
 
-if __name__=='__main__':
+    # Renderizar el gráfico como HTML
+    graph = pio.to_html(fig, full_html=False)
+
+    return render_template('home.html', graph=graph)
+
+if __name__ == '__main__':
     app.run(debug=True)
